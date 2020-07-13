3f764c8b919d49c67de571219d0f9945ae224df0
lwshang <dev@lwshang.com>
2020-07-13 16:50:44 -0400

    Import files from spring initializr

---
+++ src/main/java/team/yama/yama/YamaApplication.java
@@ -0,0 +1,13 @@
+package team.yama.yama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamaApplication.class, args);
+	}
+
+}
